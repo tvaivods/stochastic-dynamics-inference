15aa421b8b3980dc876832c6118b0bc4efaef177
tvaivods <tomass.vaivods@gmail.com>
2021-07-23 12:49:48 +0100

    Split the statistical error script into two -- data generation and data analysis

--- stat_err_analysis.py
+++ stat_err_analysis.py
@@ -17,99 +17,17 @@ import multiprocessing as mp
 import time
 from tqdm import tqdm
 
-#%% Setting up parameters and prerequisites
+#%% Loading the data
 
-# Force function
-y = lambda x : 3 - 18*x  + 12*x**2 - 2*x**3
-
-exp_lo = 3          # Shortest trajectory of length 10^exp_lo
-exp_hi = 4          # Longest trajectory of length 10^exp_hi
-n_lengths = 3      # Number of different lengths in this range
-n_strands = 10      # N. of samples per length of trajectory
-
-n_bins = 90         # Data bin number
-K = 5               # Number of CV folds
-
-ns = np.int32(10**np.linspace(exp_lo,exp_hi,n_lengths))  # Lengths of trajectories
-n_traj = 5          # Trajectories in a sample
-diffusion = 1       # Diffusion coefficient
-dt = 5e-3           # Timestep length
-
-def mp_ts(x0):      # For multiprocessing of data generation
-    np.random.seed()
-    return time_series(y,x0,dt,n,diffusion)
-
-# Specifying the basis functions
-basis1 = ps.feature_library.polynomial_library.PolynomialLibrary(degree=7)
-basis2 = ps.feature_library.fourier_library.FourierLibrary(n_frequencies=6)
-basis = basis1 + basis2
-
-suffix = f"_{exp_lo}-{exp_hi}({n_lengths}n{n_strands}s)"
-header = f"Parameters: n_bins={n_bins}, K={K}, n_traj={n_traj}, \
-diffusion={diffusion}, dt={dt}"
-
-#%% Running the algorithm
-# Produces error_list -- an array of shape [n_lengths*n_strands, n_features+1],
-# 1st column containing the trajectory length, with the latter containing the
-# errors of the strand corresponding to the n. of non-zero entries.
-
-error_list = []
-
-print(f"Running the cross-validated SSR algorithm for variable trajectory length.\n\
-Parameters:\ndt = {dt},\ndiffusion = {diffusion},\nntraj = {n_traj}\n\
-{n_lengths} different trajectory lengths ranging from {10**exp_lo} \
-to {10**exp_hi}.\n\
-Data collected in {n_bins} bins. CV performed with {K} folds of data. \n\
-Results averaged over {n_strands} runs.")
-
-time.sleep(0.5)
-pbar = tqdm(ns, leave = True)
-
-for n in pbar:                      # for each trajectory length
-    for i in range(n_strands):      # for each strand
-        pbar.set_postfix_str(f'traj_len: {n}, sample: {i+1}/{n_strands}')
-        
-        # Generating the data
-        x_multiple = []
-        x0s = np.full(n_traj, 2)
-        pool = mp.Pool()
-        x_multiple = pool.map(mp_ts, x0s)
-        pool.close()
-        pool.join()
-        
-        x_multiple = np.array(x_multiple).T
-        
-        # Computing the matrices
-        Y_multiple = ps.differentiation.FiniteDifference(order = 1)._differentiate(
-            x_multiple, dt
-        )
-        Y_single = Y_multiple.reshape(-1, 1, order = 'F')
-        x_single = x_multiple.reshape(-1, 1, order = 'F')
-        
-        # Computing the binned matrices
-        x_binned, Y_binned, weights = bin_data(x_single, Y_single, n_bins,
-                                           width_type = 'equal')
-        basis.fit(x_binned)
-        X_binned = basis.transform(x_binned)
-        W = np.diag(weights)
-        
-        # Running the SSR algorithm
-        errors = CV_scores(np.matmul(W,X_binned), np.matmul(W,Y_binned), K = K)
-        error_list.append(np.concatenate([[n], errors]))
-
-error_list = np.array(error_list)
-
-np.savetxt("stat_analysis/trajdata"+suffix+".csv", error_list, delimiter = ',', header=header)
-
-print("Done! Data saved as " + "stat_analysis/trajdata"+suffix+".csv")
-
+suffix = '_3-5(3n10s)D1.000-standard'
+error_list = np.loadtxt("stat_analysis/errdata"+suffix+".csv", delimiter = ',')
+n_lengths = len(np.unique(error_list[:,0]))
+n_strands = error_list.shape[0]/n_lengths
+n_features = error_list.shape[1]
+ns = np.unique(error_list[:,0])
 
 #%% Visualising the CV scores
 
-# Reading the data and computing mean, std for strand groups
-error_list = np.loadtxt("stat_analysis/trajdata"+suffix+".csv", delimiter = ',')
-n_lengths = len(np.unique(error_list[:,0]))
-n_strands = error_list.shape[0]/n_lengths
 mean_list = []
 std_list = []
 for i in range(n_lengths):
@@ -119,7 +37,7 @@ mean_list = np.array(mean_list)
 std_list = np.array(std_list)
 
 # Plotting the data
-fig, ax = plt.subplots(1, 2, figsize=(8, 4), gridspec_kw={'width_ratios': [20, 1]}, dpi=300)
+fig, ax = plt.subplots(1, 2, figsize=(8, 4), gridspec_kw={'width_ratios': [20, 1]}, dpi=200)
 
 cmap = pl.cm.jet
 norm = matplotlib.colors.LogNorm(vmin=np.min(error_list[:,0]),
@@ -128,7 +46,7 @@ colors = cmap(norm(error_list[:,0]).data)
 
 ax[0].set_title("CV scores for different sample sizes")
 ax[0].set_xlabel("Number of non-zero terms")
-ax[0].set_ylabel(r"CV score $\delta$")
+ax[0].set_ylabel(r"CV score $\delta^2$")
 ax[0].set_xticks(np.arange(1,error_list.shape[1],1))
 ax[0].set_xlim([1,error_list.shape[1]-1])
 
@@ -151,3 +69,49 @@ ax[1].set_ylabel("Trajectory length")
 
 fig.savefig("stat_analysis/errors"+suffix+".png")
 plt.show()
+
+#%% CV score v. Traj length for fixed n. of terms
+
+import sklearn as sk
+
+fig = plt.figure(figsize=(8, 4), dpi=200)
+
+n_terms_list = [4]
+
+reg = sk.linear_model.LinearRegression(fit_intercept = True)
+reg.fit(np.log(ns.reshape(-1,1)), np.log(mean_list))
+coeffs = reg.coef_
+intercepts = reg.intercept_
+
+for n_terms in n_terms_list:
+
+    c = coeffs[n_terms - 1]
+    a = intercepts[n_terms - 1]
+    
+    # for i in range(n_lengths):
+    #     plt.scatter(ns[i], mean_list[i,3])
+    plt.scatter(ns, mean_list[:,n_terms-1])
+    plt.yscale("log")
+    plt.xscale("log")
+    
+    plt.plot(ns, np.exp(a)*ns**c, '--', alpha = 0.5)
+
+plt.legend([rf"${n_terms}$" for n_terms in n_terms_list],
+           title = "# of non-zero terms")
+plt.ylabel("$\delta^2$")
+plt.xlabel("Trajectory length")
+
+plt.show()
+
+#%% Slope v. n. of nonzero terms
+
+fig = plt.figure(figsize=(8, 4), dpi=200)
+
+plt.scatter(np.arange(1,n_features,1), coeffs, c = 'k', s = 15)
+
+plt.title("Change in the exponent of the fit of $\delta^2 \propto n^c$")
+plt.ylabel("$Exponent c$")
+plt.xlabel("Number of non-zero terms")
+plt.xticks(np.arange(1,n_features,1))
+
+
---
+++ stat_err_data.py
@@ -0,0 +1,109 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jul 23 12:25:15 2021
+
+@author: tomass
+"""
+
+import numpy as np
+import matplotlib
+import matplotlib.pyplot as plt
+import matplotlib.pylab as pl
+from dataprep import *
+from stochastic_sindy import *
+import multiprocessing as mp
+from timeit import default_timer as timer
+from datetime import timedelta
+from tqdm import tqdm
+from bases import *
+
+y = lambda x : 3 - 18*x  + 12*x**2 - 2*x**3
+
+exp_lo = 3          # Shortest trajectory of length 10^exp_lo
+exp_hi = 5          # Longest trajectory of length 10^exp_hi
+n_lengths = 3      # Number of different lengths in this range
+n_strands = 10      # N. of samples per length of trajectory
+
+n_traj = 5          # Trajectories in a sample
+K = 5               # Number of CV folds
+diffusion = 1       # Diffusion coefficient
+dt = 5e-3           # Timestep length
+n_bins = 90
+
+basis = standard_basis
+n_features = standard_n_features
+basis_name = standard_name
+
+def mp_ts(x0, n):      # For multiprocessing of data generation
+    np.random.seed()
+    return time_series(y,x0,dt,n,diffusion)
+
+def gen_stat_err_data(y, exp_lo, exp_hi, n_lengths, n_strands, n_traj, K,
+                           diffusion, dt, n_bins, basis, n_features, basis_name):
+    
+    suffix = f"_{exp_lo}-{exp_hi}({n_lengths}n{n_strands}s)D{diffusion:.3f}-{basis_name}"
+    header = f"Parameters: n_bins={n_bins}, n_traj={n_traj},"\
+             + f" diffusion={diffusion}, dt={dt}, basis: {basis_name}"
+    
+    print(f"Generating statistical survival data for {n_lengths} trajectory lengths between "
+          + f"{10**exp_lo} and {10**exp_hi}, with {n_strands} strands per trajectory length. "
+          + f"\n{header}\n")
+    start = timer()
+    
+    ns = np.int32(10**np.linspace(exp_lo,exp_hi,n_lengths))
+    
+    error_list = []
+    
+    for n in ns:                      # for each trajectory length
+        print(f"n = {n}")
+        
+        pbar = tqdm(range(n_strands), leave = True)
+        for i in pbar:      # for each strand
+            pbar.set_postfix_str(f'Computing strand {i+1}/{n_strands}')
+            
+            # Generating the data
+            x_multiple = []
+            x0s = np.full(n_traj, 2)
+            traj_lengths = np.full(n_traj, n)
+            pool = mp.Pool()
+            mp_arg = np.transpose([x0s, traj_lengths])
+            x_multiple = pool.starmap(mp_ts, mp_arg)
+            pool.close()
+            pool.join()
+            
+            x_multiple = np.array(x_multiple).T
+            
+            # Computing the matrices
+            Y_multiple = ps.differentiation.FiniteDifference(order = 1)._differentiate(
+                x_multiple, dt
+            )
+            Y_single = Y_multiple.reshape(-1, 1, order = 'F')
+            x_single = x_multiple.reshape(-1, 1, order = 'F')
+            
+            # Computing the binned matrices
+            x_binned, Y_binned, weights = bin_data(x_single, Y_single, n_bins,
+                                               width_type = 'equal')
+            basis.fit(x_binned)
+            X_binned = basis.transform(x_binned)
+            W = np.diag(weights)
+            
+            # Running the SSR algorithm
+            errors = CV_scores(np.matmul(W,X_binned), np.matmul(W,Y_binned), K = K) # delta^2 !!!
+            error_list.append(np.concatenate([[n], errors]))
+    
+    error_list = np.array(error_list)
+    
+    end = timer()
+    print(f"\nDone! Process duration: {timedelta(seconds=end-start)}.")
+    
+    np.savetxt("stat_analysis/errdata"+suffix+".csv", error_list,
+               delimiter = ',', header=header)
+    
+    print(f"Data saved in:\nstat_analysis/errdata{suffix}.csv")
+    
+    return suffix
+
+if __name__ == '__main__':
+    gen_stat_err_data(y, exp_lo, exp_hi, n_lengths, n_strands, n_traj, K,
+                        diffusion, dt, n_bins, basis, n_features, basis_name)
